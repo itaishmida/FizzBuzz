e9cf812fb0a1adc303a878b702ea41060b500d3a
itai.shmida <itai.shmida@gmail.com>
2014-04-08 19:28:27 +0300

    RED: first init

---
+++ src/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package FizzBuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
