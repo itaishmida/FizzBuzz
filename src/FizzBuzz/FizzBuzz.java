4e9c86ad5bfcec0f550ae44d9a1199b19a59abf5
itai.shmida <itai.shmida@gmail.com>
2014-04-29 20:37:02 +0300

    GREEN: return "buzz" for value=5

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -8,6 +8,8 @@ public class FizzBuzz {
 				return String.valueOf(num);
 			else if (num % 3 == 0)
 				return "fizz";
+			else if (num == 5)
+				return "buzz";
 			else
 				return String.valueOf(num);
 				
