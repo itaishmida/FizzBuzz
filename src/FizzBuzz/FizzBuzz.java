48f16f03839c930942042c148381c862070d41ec
itai.shmida <itai.shmida@gmail.com>
2014-04-29 20:42:41 +0300

    GREEN: support for values divided by 3 and 5 to return "FizzBuzz"

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -6,6 +6,8 @@ public class FizzBuzz {
 			
 			if (num == 0)
 				return String.valueOf(num);
+			else if (num % 3 == 0 && num % 5 == 0)
+				return "FizzBuzz";
 			else if (num % 3 == 0)
 				return "fizz";
 			else if (num % 5 == 0)
